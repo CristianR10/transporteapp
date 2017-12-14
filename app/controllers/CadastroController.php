15ffc78ddd3145d6c7f3a73bac3fa6004526d532
CristianR10 <cristian_hiei@hotmail.com>
2017-12-14 17:30:52 -0200

    primeiro commit

---
+++ app/controllers/CadastroController.php
@@ -0,0 +1,25 @@
+<?php
+
+class CadastroController extends \HXPHP\System\Controller
+{
+   public function cadastrarAction()
+   {
+   		$this->view->setFile('index');
+		$this->request->setCustomFilters(array(
+			'email' => FILTER_VALIDATE_EMAIL
+		));
+
+		$post = $this->request->post();
+		
+		if (!empty($post)) {
+			$cadastrarUsuario = User::cadastrar($post);
+			if ($cadastrarUsuario->status === false) {
+				$this->load('Helpers\Alert', array(
+					'danger',
+					'Ops! Não foi possível efetuar seu cadastro. <br> Verifique os erros abaixo:',
+					$cadastrarUsuario->errors
+				));
+   			}
+   		}
+   }
+}
---
+++ app/models/User.php
@@ -0,0 +1,74 @@
+<?php
+// Model ele é o singuldar da tabela e o padrão do meu case
+
+class User extends \HXPHP\System\Model
+{
+	static $validates_presence_of = array(
+		array(
+			'name',
+			'message' => 'O nome é um campo obrigatório.'
+		),
+		array(
+			'email',
+			'message' => 'O e-mail é um campo obrigatório.'
+		),
+		array(
+			'username',
+			'message' => 'O nome de usuário é um campo obrigatório.'
+		),
+		array(
+			'password',
+			'message' => 'A senha é um campo obrigatório.'
+		)
+	);
+
+		static $validates_uniqueness_of = array(
+		array(
+			'username',
+			'message' => 'Já existe um usuário com este nome de usuário cadastrado.'
+		),
+		array(
+			'email',
+			'message' => 'Já existe um usuário com este e-mail cadastrado.'
+		)
+	);
+
+	public static function cadastrar(array $post)
+	{
+		$userObj = new \stdClass;
+		$userObj ->user = null;
+		$userObj ->status = false;
+		$userObj -> errors = array();
+
+		$role = Role::find_by_role('user');
+
+		if (is_null($role)) {
+			array_push($userObj->errors, 'A role user não existe. Contate o administrador');
+			return $userObj;
+		}
+
+		$post = array_merge($post, array(
+			'role_id' => $role->id,
+			'status' => 1
+			));
+
+		$password = \HXPHP\System\Tools::hashHX($post['password']);		
+
+		$post = array_merge($post, $password);
+
+		$cadastrar = self::create($post);
+
+		if($cadastrar->is_valid()){
+			$userObj->user = $cadastrar;
+			$userObj->status = true;
+			return $userObj;
+		}
+
+		$errors = $cadastrar->errors->get_raw_errors();
+		
+		foreach ($errors as $field => $message){
+			array_push($userObj->errors, $message[0]);
+		}
+		return $userObj;
+	}
+}

c9a5100ed8504e2b7abfcb455e7c8569db090d6a
CristianR10 <cristian_hiei@hotmail.com>
2018-01-06 15:56:33 -0200

    Atualização do menu / js.materialize

--- app/config.php
+++ app/config.php
@@ -1,17 +1,29 @@
 <?php
-  //Constantes
+  //Constantes  
+  /* *******OBS DUVIDAS ASSISTIR A AULA 14 
+  https://www.youtube.com/watch?v=VY6-8kj2frI&list=PL2NLqGvZxQu2xMnvPKqLz4f9Pljsj-K4B&index=15*/
+
   $configs = new HXPHP\System\Configs\Config;
+  /*Configuração dos ambientes para isso ele precisa existir na pasta
+  src/HXPHP/System/configs/Environments duvidas assistir a aula 2 
+  https://www.youtube.com/watch?v=sr_awqysIn8&list=PL2NLqGvZxQu2xMnvPKqLz4f9Pljsj-K4B&index=2
+  */
+
   $configs->env->add('development');
   $configs->env->development->baseURI = '/git/transporte/';
+  /*variavel config conecta com env = environment = ambiente configura a URI*/
+
   $configs->env->development->database->setConnectionData(array(
     'host' => 'localhost',
     'user' => 'root',
     'password' => '',
     'dbname' => 'transporteapp'
   ));
+  //Conecxão do ambiente ao BD
+
   $configs->env->development->auth->setURLs('/git/transporte/home', '/git/transporte/login');
   $configs->env->development->menu->setMenus(array(
-    'Home/dashboard' => '%baseURI%/home',
+    'Home/fa-dashboard' => '%baseURI%/home',
     'Editar perfil/cog' => '%baseURI%/perfil/editar',
     'Sair/sign-out' => '%baseURI%/login/sair',
   ), 'user');
@@ -24,11 +36,13 @@
   $configs->env->development->menu->setMenus(array(
     'Home/dashboard' => '%baseURI%/home'
   ));
+  //Mudar 
   $configs->env->development->menu->setConfigs(array(
     'container' => 'nav',
     'container_class' => 'navbar navbar-default',
     'menu_class' => 'nav navbar-nav'
   ));
+
   $configs->env->add('production');
   $configs->env->production->baseURI = '/';
   $configs->env->production->database->setConnectionData(array(
@@ -39,6 +53,7 @@
   ));
   $configs->env->production->auth->setURLs('/home/', '/login/');
   $configs->env->production->menu->setMenus(array(
+  //texto do menu/
     'Home/dashboard' => '%baseURI%/home',
     'Editar perfil/cog' => '%baseURI%/perfil/editar',
     'Sair/sign-out' => '%baseURI%/login/sair',
@@ -57,4 +72,5 @@
     'container_class' => 'navbar navbar-default',
     'menu_class' => 'nav navbar-nav'
   ));
+  //Modifica a navbar mas ainda não sei onde coloca o js do materialise
   return $configs;

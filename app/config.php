18e68d474a4703da0686631cd0023ce3b3283f2e
CristianR10 <cristian_hiei@hotmail.com>
2018-01-16 19:36:00 -0200

    Manutenção do Nav bar

--- app/config.php
+++ app/config.php
@@ -2,17 +2,14 @@
   //Constantes  
   /* *******OBS DUVIDAS ASSISTIR A AULA 14 
   https://www.youtube.com/watch?v=VY6-8kj2frI&list=PL2NLqGvZxQu2xMnvPKqLz4f9Pljsj-K4B&index=15*/
-
   $configs = new HXPHP\System\Configs\Config;
   /*Configuração dos ambientes para isso ele precisa existir na pasta
   src/HXPHP/System/configs/Environments duvidas assistir a aula 2 
   https://www.youtube.com/watch?v=sr_awqysIn8&list=PL2NLqGvZxQu2xMnvPKqLz4f9Pljsj-K4B&index=2
   */
-
   $configs->env->add('development');
   $configs->env->development->baseURI = '/git/transporte/';
   /*variavel config conecta com env = environment = ambiente configura a URI*/
-
   $configs->env->development->database->setConnectionData(array(
     'host' => 'localhost',
     'user' => 'root',
@@ -20,7 +17,6 @@
     'dbname' => 'transporteapp'
   ));
   //Conecxão do ambiente ao BD
-
   $configs->env->development->auth->setURLs('/git/transporte/home', '/git/transporte/login');
   $configs->env->development->menu->setMenus(array(
     'Home/fa-dashboard' => '%baseURI%/home',
@@ -42,7 +38,6 @@
     'container_class' => 'navbar navbar-default',
     'menu_class' => 'nav navbar-nav'
   ));
-
   $configs->env->add('production');
   $configs->env->production->baseURI = '/';
   $configs->env->production->database->setConnectionData(array(
